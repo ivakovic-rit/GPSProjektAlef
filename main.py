3f8497c026e14bfca77436df1c413a4ad5f9b021
Marino Ivakovic <marino.ivakovic@rit.de>
2023-09-19 14:14:05 +0200

    http hochpuschen

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import gpxpy
 import gpxpy.gpx
 import os
-from flask import Flask
+from flask import Flask, request, render_template
 from db_requests import *
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template', static_folder='static')
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
-def main():
-    counter = 0
-    directory = "gpx-Dateien"
-    for filename in os.listdir(directory):
-        if not filename.endswith(".gpx") or isTrackInDatabase(filename): continue
-        gpx_file = open(directory + "/" + filename, 'r')
-        saveTrackRecordToDb(gpx_file, filename)
-        counter += 1
-
-    print(f'Anzahl der Dateien: {counter}')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        kfz = request.form.get('kfz')
+        von_datum = request.form.get('von_datum')
+        bis_datum = request.form.get('bis_datum')
+        # You can perform any logic or processing with these values here
+        
+        # For now, just printing them to the console
+        print(f'Name: {name}, KFZ: {kfz}, Von Datum: {von_datum}, Bis Datum: {bis_datum}')
+    
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
+
+# def main():
+#     counter = 0
+#     directory = "gpx-Dateien"
+#     for filename in os.listdir(directory):
+#         if not filename.endswith(".gpx") or isTrackInDatabase(filename): continue
+#         gpx_file = open(directory + "/" + filename, 'r')
+#         saveTrackRecordToDb(gpx_file, filename)
+#         counter += 1
+
+#     print(f'Anzahl der Dateien: {counter}')
+
+# if __name__ == '__main__':
+#     main()
